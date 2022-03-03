6d10d2b82b0d8e07b0b7243dc2ccfe8b6a3553d7
Prachi1808 <prachimohanty.1808@gmail.com>
2022-03-03 07:41:32 +0530

    Generics maximum finder

---
+++ src/UC1.java
@@ -0,0 +1,17 @@
+
+public class UC1 {
+public static Integer findMax(Integer int1, Integer int2, Integer int3) 
+ {
+	        Integer max = int1;
+	        if (int2.compareTo(max) > 0) {
+	            max = int2;
+	        }
+	        if (int3.compareTo(max) > 0) {
+	            max = int3;
+	        }
+	        System.out.printf("Max values of %s %s %s is %s \n", int1, int2, int3, max);
+	        return max;
+	    }
+		
+	}
+
